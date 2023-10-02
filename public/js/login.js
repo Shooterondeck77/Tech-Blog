ff5addc822a98fa47f30b9a532bfb04df69cb82f
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-01 20:12:08 -0400

    added login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,40 @@
 // We would GRAB reference to the HTML ELEMENT ON the HTML page
 // CAPTURE the VALUES
 // --> Send the Data to our SERVER API
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+
+
+
+
+
+
+
+
+
+
+
 
 console.log("Login JS Connected...")

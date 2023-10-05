4b5eb34e313e0d20e94cca9442eddd605ea54923
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-05 15:23:04 -0400

    setup core layout

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,18 +1,11 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
-const withAuth = require('../auth/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    res.render("homepage")
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
       include: [
         {
           model: User,
@@ -21,10 +14,34 @@ router.get('/project/:id', async (req, res) => {
       ],
     });
 
-    const project = projectData.get({ plain: true });
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('project', {
-      ...project,
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('single-post', {
+      ...post,
       logged_in: req.session.logged_in
     });
   } catch (err) {
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./api/user-routes');
-const postRoutes = require('./api/post-routes');
-const commentRoutes = require('./api/comment-routes');
-const homeRoutes = require("./homeRoutes")
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
-router.use('/', homeRoutes)
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
-
-router.get('/login', (req, res) => {
-    console.log("Hit Login Route");
-
-    res.render('login');
-});
-
-router.get('/dashboard', (req, res) => {
-    console.log("Hit Dashboard Route");
-    const user = "Bill"
-    const num = 52
-    const usernames = ["Bill", "Marco", "John", "Sarah"]
-
-    res.render('dashboard', { data: user, dataNum: num, arr: usernames });
-});
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
